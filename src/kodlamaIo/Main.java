3490b2c5aff16e1b5883da0b31b246c32cfd588d
ertanciller <ertanciller@gmail.com>
2022-10-12 19:23:40 +0300

    deneme eklendi

--- src/kodlamaIo/Main.java
+++ src/kodlamaIo/Main.java
@@ -41,6 +41,7 @@ public class Main {
 		courseManager.add(course2);
 		System.out.println("************************************");
 
+		System.out.println("deneme 2");
 		
 		CategoriesManager categoryManager = new CategoriesManager(new JdbcCategoryDao(),logger);
 		categoryManager.add(category);
